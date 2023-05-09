a4a0e7cbedd418bd8378b3358a1593f4596bc693
VishwanathDinnesh <132496394+VishwanathDinnesh@users.noreply.github.com>
2023-05-09 22:48:12 +0530

    Config file Updated

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ module.exports = defineConfig({
      *  Maximum time expect() should wait for the condition to be met.
      * For example in 'await expect(locator).toHaveText();
      */
-    timeout: 90000
+    timeout: 120000
   },
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
