100abe55c49867b78b58785895942dd57c2c4e94
VishwanathDinnesh <132496394+VishwanathDinnesh@users.noreply.github.com>
2023-05-19 23:02:54 +0530

    OrderStatusGoya

--- tests/Orderstatus.spec.js
+++ tests/Orderstatus.spec.js
@@ -25,36 +25,37 @@ test('Order Status', async ({ page }) => {
     //Addd assertions to verify the URL of the home page .
     await expect(page).toHaveURL('https://portal-test.goya.com/oms2/#/home');
 
-    await page.getByRole('button', { name: 'Order', exact: true }).click();
-    await page.getByRole('searchbox', { name: 'Customer' }).click();
-    await page.getByText('712457-RUMBA CUBANA OF TONNELLE CORP').click();
-    
-    await page.getByRole('textbox', { name: 'Item / UPC#' }).click();
-    await page.getByRole('textbox', { name: 'Item / UPC#' }).fill('2365');
-    await page.getByPlaceholder('Cases').press('Tab');
-    await page.getByRole('textbox', { name: 'Item / UPC#' }).fill('2465');
-    await page.getByPlaceholder('Cases').press('Tab');
-    await page.getByRole('link', { name: 'Order Status' }).click();
-
+    await page.getByRole('button', { name: 'Order Status' }).click();
     await page.waitForTimeout(11000)
-    await page.getByRole('cell', { name: '356303352 (W)' }).click();
+    await page.getByRole('searchbox', { name: 'Status' }).click();
+    await page.getByRole('button', { name: 'Status focus' }).press('ScrollLock');
+    await page.getByRole('option', { name: 'Open', exact: true }).getByText('Open').click();
+    await page.waitForTimeout(11000)
+    await page.getByRole('cell', { name: '356303375 (W)' }).click();
     await page.screenshot({ path: "Pdf.png", fullPage: true })
     const downloadPromise = page.waitForEvent('download');
     await page.locator('#pdf').getByText('PDF').click();
     const download = await downloadPromise;
     page.once('dialog', dialog => {
-    console.log(`Dialog message: ${dialog.message()}`);
-    dialog.dismiss().catch(() => {});
-    });
+      console.log(`Dialog message: ${dialog.message()}`);
+      dialog.dismiss().catch(() => {});
+    })
     await page.locator('#pdf').getByText('Email').click();
     await page.locator('#pdf').getByText('Close').click();
-    await page.locator('.table > tbody > tr > td:nth-child(3)').first().click();
-    await page.screenshot({ path: "Popup.png", fullPage: true})
-    await page.getByRole('button', { name: 'Close' }).click();
-    
     await page.locator('.delete-icon').first().click();
-    await page.screenshot({ path: "Cancel.png", fullPage: true})
+    await page.screenshot({ path: "Cancel.png", fullPage: true })
     await page.getByRole('button', { name: 'No' }).click();
+    await page.locator('.table > tbody > tr > td:nth-child(3)').first().click();
+    await page.waitForTimeout(1000)
+    await page.screenshot({ path: "Popup.png", fullPage: true })
+    await page.getByRole('button', { name: 'Close' }).click();
+    await page.waitForTimeout(5000)
+    
+    await page.getByRole('searchbox', { name: 'Customer' }).click();
+    await page.getByText('712457-RUMBA CUBANA OF TONNELLE CORP').click();
+    // await page.getByRole('checkbox', { name: 'Need Attention' }).check();
+    // await page.getByRole('checkbox', { name: 'Need Attention' }).uncheck();
+
     
     await page.getByRole('listitem').filter({ hasText: 'Home' }).getByRole('link', { name: 'Home' }).click();
     await page.getByRole('button', { name: '013506-CARLOS MORATO' }).click();
